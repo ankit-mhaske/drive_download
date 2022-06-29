f606c6887f37f52161c17d804c69ab81acee3c44
ankit-mhaske <108428056+ankit-mhaske@users.noreply.github.com>
2022-06-30 00:24:52 +0530

    update changes

--- redhat.py
+++ redhat.py
@@ -6,10 +6,6 @@ auth = GoogleAuth() #authorise application
 drive_obj = GoogleDrive(auth)
 folder = "18vTzaKFVoCs8eHAZ9_bI1aXdI4beDNfx"
 
-"""file1 = drive_obj.CreateFile({'parents':[{'id':folder}],'title':'hi.txt'})
-file1.SetContentString('hello')
-file1.Upload()
-"""
 file_names = drive_obj.ListFile({'q':f"'{folder}' in parents and trashed=false"}).GetList()
 for files in file_names:
 	files.GetContentFile(files['title'])
